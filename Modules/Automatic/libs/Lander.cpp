01069017de0832235fec77f6b4c115add1f78643
Andrea Nistico <andrea.nistic@gmail.com>
2017-05-22 16:48:45 +0200

    fix pid constructor

--- Modules/Automatic/libs/Lander.cpp
+++ Modules/Automatic/libs/Lander.cpp
@@ -11,7 +11,7 @@ Lander::Lander()
         : _horizontaErr((double)0)    , _tauHold((double)0), _tauLost((double)0), _tauErr((double)0), _NHold(0),
           _NLost(0),_NComp(0), _initS(&_machine), _holdS(&_machine)  , _asceS(&_machine)  , _descS(&_machine),_compS(&_machine),
           _rtolS(&_machine),_landS(&_machine), _err(0,0,0), _err_int(0,0,0), _err_diff(0,0,0), _dt(0), _prevTime(0), _actualTime(0),_actualState(0),_prevState(0),
-          _verticalErr(0), _holdPIDX(params_automatic::KpHold,0,params_automatic::KiHold), _holdPIDY(params_automatic::KdHold,0,params_automatic::KiHold)
+          _verticalErr(0), _holdPIDX(params_automatic::KpHold,params_automatic::KiHold,0), _holdPIDY(params_automatic::KdHold,params_automatic::KiHold,0)
 {
 
     initStateMachine();

1e34e7d380a997efbb5c9db6e0143d5c14d7eefd
Andrea Nistico <andrea.nistic@gmail.com>
2017-10-23 16:18:35 +0200

    fix configure script

--- configure.sh
+++ configure.sh
@@ -3,7 +3,7 @@
 sudo apt-get install libglib2.0-dev &&
 sudo apt-get install libeigen3-dev &&
 sudo apt-get install build-essential &&
-cd projectLIBS/lcm &&
+cd libs/lcm &&
 ./bootstrap.sh &&
 ./configure &&
 make &&
